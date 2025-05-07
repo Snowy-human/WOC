877fb81fc411ef5f674ea9aacdb19013987f2de6
Ansony D. M. <willow.26122007.tree@gmail.com>
2025-05-07 13:06:05 -0700

    Add Entity needs more work

---
+++ Entity.gd
@@ -0,0 +1,10 @@
+extends Resource
+
+class_name Entity
+
+## name of the entity
+@export var Name := "Null"
+## the stats will be a diffent class so it can be reused
+@export var Stats := "res://Player/Player base/Base Player.tres"
+## the textures will be able to be longer
+@export var Textures := 100.0
